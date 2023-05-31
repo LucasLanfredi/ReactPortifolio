328d3423bf89b42bbee62c89aa28784c07a5bf14
LucasLanfredi <lucas-lanfredi@hotmail.com.br>
2023-05-31 15:07:27 -0300

    Tradução e CSS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
+import React, { useState, useEffect } from 'react';
+import { useTranslation, I18nextProvider } from 'react-i18next';
 import i18n from 'i18next';
 import Header from './Header';
 import Summary from './Summary';
 import Projects from './Projects';
 
+
 function App() {
+  const [selectedLanguage, setSelectedLanguage] = useState('pt');
   const { t } = useTranslation();
 
   const projects = [
@@ -17,13 +19,27 @@ function App() {
       titleKey: 'projects.project2.title',
       descriptionKey: 'projects.project2.description',
     },
+    {
+      titleKey: 'projects.project3.title',
+      descriptionKey: 'projects.project3.description',
+    },
   ];
 
+  const changeLanguage = (lang) => {
+    setSelectedLanguage(lang);
+  };
+
+  useEffect(() => {
+    i18n.changeLanguage(selectedLanguage);
+  }, [selectedLanguage]);
+
   return (
+    
     <div>
       <Header
         greeting={t('header.greeting')}
         jobTitle={t('header.jobTitle')}
+        onChangeLanguage={changeLanguage}
       />
       <Summary professionalSummary={t('summary.professionalSummary')} />
       <Projects t={t} projects={projects} />
@@ -44,4 +60,12 @@ i18n.init({
   }
 });
 
-export default App;
+function RootApp() {
+  return (
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
+  );
+}
+
+export default RootApp;
--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { changeLanguage } from 'i18next';
 
-function Header({ greeting, jobTitle }) {
+function Header({ greeting, jobTitle, onChangeLanguage }) {
+
   return (
     <header>
+        <button onClick={() => changeLanguage('en')}>English</button>
+        <button onClick={() => changeLanguage('pt')}>Português</button>
       <p>{greeting}</p>
       <h1>{jobTitle}</h1>
     </header>
